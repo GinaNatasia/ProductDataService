53f44037ce540cd79c25cedca2785c6e0e3b2b31
Gina Slager <g.slager@brunel.net>
2025-06-20 16:19:50 +0200

    Initial commit

---
+++ ProductDataService/src/main/java/com/gigaia/ProductDataService/ProductDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gigaia.ProductDataService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductDataServiceApplication.class, args);
+	}
+
+}
